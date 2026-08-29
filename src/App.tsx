a0b717eb77d36f0bb86944c8b87ca3280302b1fa
Inferno-RE <rfz@udel.edu>
2026-08-29 15:49:00 -0400

    framework of website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
+            <div className="redstuff" style={{ backgroundColor: "red" }}></div>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -11,8 +13,27 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p>Runfeng Zhou edited.
-            </p>
+            <p>Runfeng Zhou edited.</p>
+            <h1>Website by rfz</h1>
+            <p>the testing page</p>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+                alt="Cat pic"
+                width="200"
+            />
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
         </div>
     );
 }
